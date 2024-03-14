9999b09fbc2031baf4cf07e958700e0258ef70e3
zowiedrid <102651464+zowiedrid@users.noreply.github.com>
2024-03-14 17:37:52 +0700

    mengcopy void input dari projek sebeumnya dan memindahkan array dan n ke global

--- prk2algo_insertion/prk2algo_insertion.cpp
+++ prk2algo_insertion/prk2algo_insertion.cpp
@@ -4,25 +4,50 @@
 #include <iostream>
 using namespace std;
 
-void insertionSort() {
-    int i, n, temp, j; //deklarasi variabel
-    int arr[9]; //deklarasi array
+int arr[10]; // jumlah kapasitas array
+int n; // jumlah data
 
-    for (i = 1; i <= n - 1; i++) { // step1
-        temp = arr[i]; // step2
-        j = i - 1; // step3
+void input() {
+	while (true) {
+		cout << "Masukkan jumlah data: "; //output ke layar
+		cin >> n; //input dari user
+		if (n <= 10) { //jika n kurang dari sama dengan 10
+			break; //keluar dari loop
+		}
+		else {
+			cout << "Jumlah data tidak boleh lebih dari 10" << endl; //output ke layar
+		}
+	}
 
-        while (j >= 0 && arr[j] > temp) { //step4
-        
-        arr[j + 1] = arr[j]; //step4a nilai di kanan berarti akan diisi oleh nilai di kiri
-        j--; //step4b }
+	cout << endl; //output ke layar
+	cout << "=============================" << endl; //output ke layar
+	cout << "Masukkan data: " << endl; //output ke layar
+	cout << "=============================" << endl; //output ke layar
 
-        arr[j + 1] = temp; //step5
-    }
+	for (int i = 0; i < n; i++) { //looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << i + 1 << ": "; //output ke layar
+		cin >> arr[i]; //input dari user
+	}
 }
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void insertionSort() {
+	int i, temp, j; //deklarasi variabel
+	for (i = 1; i <= n - 1; i++) { // step1
+		temp = arr[i]; // step2
+		j = i - 1; // step3
+
+		while (j >= 0 && arr[j] > temp) { //step4
+
+			arr[j + 1] = arr[j]; //step4a nilai di kanan berarti akan diisi oleh nilai di kiri
+			j--; //step4b }
+
+			arr[j + 1] = temp; //step5
+		}
+	}
+
+}
+
+int main() {
+
 }
 
